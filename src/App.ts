ea13252e435bbc1a4543b64fd2dd0cda7af661d5
JimJ92120 <jimj.development@gmail.com>
2025-04-08 07:54:06 +0700

    feat(app): add 1..9 button controls

--- src/App.ts
+++ src/App.ts
@@ -16,6 +16,43 @@ class App {
   <p>
     <pre id="map"></pre>
   </p>
+
+  <div class="controls">
+    ${[...Array(9).keys()].reduce(
+      (_result, index) =>
+        _result +
+        `<div class="controls__button" data-value="${index + 1}">${
+          index + 1
+        }</div>`,
+      ""
+    )}
+  </div>\
+
+  <style>
+    #scene {
+      cursor: pointer;
+    }
+    .controls {
+      display: flex;
+      flex-flow: row wrap;
+      max-width: 300px;
+      margin-left: auto;
+      margin-right: auto;
+    }
+    .controls__button {
+      display: inline;
+      width: calc((100% - 3rem) / 3);
+      box-sizing: border-box;
+      border: 1px solid black;
+      margin: 0.5rem;
+      padding: 0.5rem;
+      cursor: pointer;
+    }
+    .controls__button--selected {
+      border: 1px solid green;
+      color: green;
+    }
+  </style>
     `;
   }
 }
